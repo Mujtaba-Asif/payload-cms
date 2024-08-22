dda5c83a55d93a1ad75a0b505aeed3b9d26fbd88
Mujtaba-Asif <mujtaba@earlybirdlabs.io>
2024-08-22 11:38:19 +0500

    payload demo app

---
+++ src/collections/Blogs.ts
@@ -0,0 +1,135 @@
+import { slateEditor } from "@payloadcms/richtext-slate";
+import { CollectionConfig, Block } from "payload/types";
+
+const Content: Block = {
+  slug: "contentBlock",
+  fields: [
+    {
+      name: "content",
+      type: "richText",
+      defaultValue: [
+        {
+          children: [{ text: "Here is some default content for this field" }],
+        },
+      ],
+      required: true,
+      editor: slateEditor({
+        admin: {
+          elements: [
+            "h2",
+            "h3",
+            "h4",
+            "link",
+            "blockquote",
+            "textAlign",
+            "upload",
+            "indent",
+            "ol",
+            "ul",
+          ],
+          leaves: ["bold", "italic", "code", "underline", "strikethrough"],
+          link: {
+            fields: [
+              {
+                name: "rel",
+                label: "Rel Attribute",
+                type: "select",
+                hasMany: true,
+                options: ["noopener", "noreferrer", "nofollow"],
+              },
+            ],
+          },
+          upload: {
+            collections: {
+              media: {
+                fields: [
+                  {
+                    type: "upload",
+                    name: "media",
+                    relationTo: "media",
+                    label: "Image",
+                  },
+                ],
+              },
+            },
+          },
+        },
+      }),
+    },
+  ],
+};
+
+const Media: Block = {
+  slug: "image",
+  fields: [
+    {
+      type: "upload",
+      name: "image",
+      label: "Image",
+      relationTo: "media",
+    },
+    {
+      type: "text",
+      name: "altText",
+      label: "Alt Text",
+    },
+  ],
+};
+const Code: Block = {
+  slug: "code",
+  fields: [
+    {
+      name: "code",
+      type: "code",
+      label: "Code",
+    },
+  ],
+};
+
+const Blogs: CollectionConfig = {
+  slug: "blogs",
+  fields: [
+    {
+      name: "title",
+      type: "text",
+      label: "Title",
+      required: true,
+    },
+    {
+      name: "description",
+      type: "textarea",
+      label: "Description",
+      required: true,
+    },
+    {
+      name: "coverImage",
+      type: "upload",
+      label: "Cover Image",
+      required: true,
+      relationTo: "media",
+    },
+    {
+      type: "text",
+      name: "authorName",
+      label: "Author Name",
+      required: true,
+    },
+
+    {
+      type: "upload",
+      name: "authorProfilePictutre",
+      label: "Author Profile Picture",
+      relationTo: "media",
+    },
+    {
+      name: "content",
+      label: "Content",
+      type: "blocks",
+      minRows: 1,
+      maxRows: 20,
+      blocks: [Content, Media, Code],
+    },
+  ],
+};
+
+export default Blogs;
---
+++ src/collections/Users.ts
@@ -0,0 +1,32 @@
+import { CollectionConfig } from "payload/types";
+
+const Users: CollectionConfig = {
+  slug: "users",
+  auth: true,
+  admin: {
+    useAsTitle: "email",
+  },
+  fields: [
+    {
+      name: "name",
+      type: "text",
+      required: true,
+    },
+    // {
+    //   name: "dob",
+    //   type: "date",
+    //   required: true,
+    // },
+    // {
+    //   name: "address",
+    //   type: "textarea",
+    // },
+    // {
+    //   name: "profilePicture",
+    //   type: "upload",
+    //   relationTo: "media",
+    // },
+  ],
+};
+
+export default Users;
